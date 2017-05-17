7f61f6aeded250117b037ae8e2da4874438b58b1
payton <payton@PAYTON>
2017-05-16 21:23:14 -0800

    importing projegt

---
+++ Torchhhhhhh/Form1.Designer.cs
@@ -0,0 +1,303 @@
+﻿namespace Torchhhhhhh
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.metroLink1 = new MetroFramework.Controls.MetroLink();
+            this.metroLink2 = new MetroFramework.Controls.MetroLink();
+            this.metroTextBox1 = new MetroFramework.Controls.MetroTextBox();
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.metroLink3 = new MetroFramework.Controls.MetroLink();
+            this.metroLink4 = new MetroFramework.Controls.MetroLink();
+            this.metroTabControl1 = new MetroFramework.Controls.MetroTabControl();
+            this.metroButton2 = new MetroFramework.Controls.MetroButton();
+            this.metroButton3 = new MetroFramework.Controls.MetroButton();
+            this.metroLink5 = new MetroFramework.Controls.MetroLink();
+            this.metroTextBox2 = new MetroFramework.Controls.MetroTextBox();
+            this.metroTextBox3 = new MetroFramework.Controls.MetroTextBox();
+            this.metroLink6 = new MetroFramework.Controls.MetroLink();
+            this.SuspendLayout();
+            // 
+            // metroLink1
+            // 
+            this.metroLink1.Image = ((System.Drawing.Image)(resources.GetObject("metroLink1.Image")));
+            this.metroLink1.ImageSize = 32;
+            this.metroLink1.Location = new System.Drawing.Point(14, 63);
+            this.metroLink1.Name = "metroLink1";
+            this.metroLink1.Size = new System.Drawing.Size(33, 23);
+            this.metroLink1.TabIndex = 0;
+            this.metroLink1.UseSelectable = true;
+            this.metroLink1.Click += new System.EventHandler(this.metroLink1_Click);
+            // 
+            // metroLink2
+            // 
+            this.metroLink2.Image = ((System.Drawing.Image)(resources.GetObject("metroLink2.Image")));
+            this.metroLink2.ImageSize = 32;
+            this.metroLink2.Location = new System.Drawing.Point(53, 63);
+            this.metroLink2.Name = "metroLink2";
+            this.metroLink2.Size = new System.Drawing.Size(33, 23);
+            this.metroLink2.TabIndex = 1;
+            this.metroLink2.UseSelectable = true;
+            this.metroLink2.Click += new System.EventHandler(this.metroLink2_Click);
+            // 
+            // metroTextBox1
+            // 
+            this.metroTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            // 
+            // 
+            // 
+            this.metroTextBox1.CustomButton.Image = null;
+            this.metroTextBox1.CustomButton.Location = new System.Drawing.Point(363, 1);
+            this.metroTextBox1.CustomButton.Name = "";
+            this.metroTextBox1.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.metroTextBox1.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroTextBox1.CustomButton.TabIndex = 1;
+            this.metroTextBox1.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.metroTextBox1.CustomButton.UseSelectable = true;
+            this.metroTextBox1.CustomButton.Visible = false;
+            this.metroTextBox1.Lines = new string[] {
+        "metroTextBox1"};
+            this.metroTextBox1.Location = new System.Drawing.Point(134, 63);
+            this.metroTextBox1.MaxLength = 32767;
+            this.metroTextBox1.Name = "metroTextBox1";
+            this.metroTextBox1.PasswordChar = '\0';
+            this.metroTextBox1.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.metroTextBox1.SelectedText = "";
+            this.metroTextBox1.SelectionLength = 0;
+            this.metroTextBox1.SelectionStart = 0;
+            this.metroTextBox1.ShortcutsEnabled = true;
+            this.metroTextBox1.Size = new System.Drawing.Size(385, 23);
+            this.metroTextBox1.TabIndex = 2;
+            this.metroTextBox1.Text = "metroTextBox1";
+            this.metroTextBox1.UseSelectable = true;
+            this.metroTextBox1.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.metroTextBox1.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            this.metroTextBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.metroTextBox1_KeyPress);
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.metroButton1.Location = new System.Drawing.Point(640, 63);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(57, 23);
+            this.metroButton1.TabIndex = 3;
+            this.metroButton1.Text = "Add ";
+            this.metroButton1.UseSelectable = true;
+            this.metroButton1.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // metroLink3
+            // 
+            this.metroLink3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.metroLink3.Image = ((System.Drawing.Image)(resources.GetObject("metroLink3.Image")));
+            this.metroLink3.Location = new System.Drawing.Point(525, 63);
+            this.metroLink3.Name = "metroLink3";
+            this.metroLink3.Size = new System.Drawing.Size(36, 23);
+            this.metroLink3.TabIndex = 4;
+            this.metroLink3.UseSelectable = true;
+            this.metroLink3.Click += new System.EventHandler(this.metroLink3_Click);
+            // 
+            // metroLink4
+            // 
+            this.metroLink4.Image = ((System.Drawing.Image)(resources.GetObject("metroLink4.Image")));
+            this.metroLink4.ImageSize = 20;
+            this.metroLink4.Location = new System.Drawing.Point(92, 63);
+            this.metroLink4.Name = "metroLink4";
+            this.metroLink4.Size = new System.Drawing.Size(36, 23);
+            this.metroLink4.TabIndex = 5;
+            this.metroLink4.UseSelectable = true;
+            this.metroLink4.Click += new System.EventHandler(this.metroLink4_Click);
+            // 
+            // metroTabControl1
+            // 
+            this.metroTabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.metroTabControl1.Location = new System.Drawing.Point(3, 121);
+            this.metroTabControl1.Name = "metroTabControl1";
+            this.metroTabControl1.Size = new System.Drawing.Size(756, 485);
+            this.metroTabControl1.TabIndex = 6;
+            this.metroTabControl1.UseSelectable = true;
+            // 
+            // metroButton2
+            // 
+            this.metroButton2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.metroButton2.Location = new System.Drawing.Point(567, 63);
+            this.metroButton2.Name = "metroButton2";
+            this.metroButton2.Size = new System.Drawing.Size(57, 23);
+            this.metroButton2.TabIndex = 7;
+            this.metroButton2.Text = "GO";
+            this.metroButton2.UseSelectable = true;
+            this.metroButton2.Click += new System.EventHandler(this.metroButton2_Click);
+            // 
+            // metroButton3
+            // 
+            this.metroButton3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.metroButton3.Location = new System.Drawing.Point(702, 63);
+            this.metroButton3.Name = "metroButton3";
+            this.metroButton3.Size = new System.Drawing.Size(57, 23);
+            this.metroButton3.TabIndex = 8;
+            this.metroButton3.Text = "Remove";
+            this.metroButton3.UseSelectable = true;
+            this.metroButton3.Click += new System.EventHandler(this.metroButton3_Click);
+            // 
+            // metroLink5
+            // 
+            this.metroLink5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.metroLink5.Image = ((System.Drawing.Image)(resources.GetObject("metroLink5.Image")));
+            this.metroLink5.ImageSize = 20;
+            this.metroLink5.Location = new System.Drawing.Point(640, 34);
+            this.metroLink5.Name = "metroLink5";
+            this.metroLink5.Size = new System.Drawing.Size(36, 23);
+            this.metroLink5.TabIndex = 9;
+            this.metroLink5.UseSelectable = true;
+            // 
+            // metroTextBox2
+            // 
+            this.metroTextBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            // 
+            // 
+            // 
+            this.metroTextBox2.CustomButton.Image = null;
+            this.metroTextBox2.CustomButton.Location = new System.Drawing.Point(72, 1);
+            this.metroTextBox2.CustomButton.Name = "";
+            this.metroTextBox2.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.metroTextBox2.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroTextBox2.CustomButton.TabIndex = 1;
+            this.metroTextBox2.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.metroTextBox2.CustomButton.UseSelectable = true;
+            this.metroTextBox2.CustomButton.Visible = false;
+            this.metroTextBox2.Lines = new string[] {
+        "click to add tab"};
+            this.metroTextBox2.Location = new System.Drawing.Point(548, 34);
+            this.metroTextBox2.MaxLength = 32767;
+            this.metroTextBox2.Name = "metroTextBox2";
+            this.metroTextBox2.PasswordChar = '\0';
+            this.metroTextBox2.ReadOnly = true;
+            this.metroTextBox2.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.metroTextBox2.SelectedText = "";
+            this.metroTextBox2.SelectionLength = 0;
+            this.metroTextBox2.SelectionStart = 0;
+            this.metroTextBox2.ShortcutsEnabled = true;
+            this.metroTextBox2.Size = new System.Drawing.Size(94, 23);
+            this.metroTextBox2.TabIndex = 11;
+            this.metroTextBox2.Text = "click to add tab";
+            this.metroTextBox2.UseSelectable = true;
+            this.metroTextBox2.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.metroTextBox2.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // metroTextBox3
+            // 
+            this.metroTextBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            // 
+            // 
+            // 
+            this.metroTextBox3.CustomButton.Image = null;
+            this.metroTextBox3.CustomButton.Location = new System.Drawing.Point(88, 1);
+            this.metroTextBox3.CustomButton.Name = "";
+            this.metroTextBox3.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.metroTextBox3.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroTextBox3.CustomButton.TabIndex = 1;
+            this.metroTextBox3.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.metroTextBox3.CustomButton.UseSelectable = true;
+            this.metroTextBox3.CustomButton.Visible = false;
+            this.metroTextBox3.Lines = new string[] {
+        "click to remove tab"};
+            this.metroTextBox3.Location = new System.Drawing.Point(600, 92);
+            this.metroTextBox3.MaxLength = 32767;
+            this.metroTextBox3.Name = "metroTextBox3";
+            this.metroTextBox3.PasswordChar = '\0';
+            this.metroTextBox3.ReadOnly = true;
+            this.metroTextBox3.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.metroTextBox3.SelectedText = "";
+            this.metroTextBox3.SelectionLength = 0;
+            this.metroTextBox3.SelectionStart = 0;
+            this.metroTextBox3.ShortcutsEnabled = true;
+            this.metroTextBox3.Size = new System.Drawing.Size(110, 23);
+            this.metroTextBox3.TabIndex = 12;
+            this.metroTextBox3.Text = "click to remove tab";
+            this.metroTextBox3.UseSelectable = true;
+            this.metroTextBox3.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.metroTextBox3.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // metroLink6
+            // 
+            this.metroLink6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.metroLink6.Image = ((System.Drawing.Image)(resources.GetObject("metroLink6.Image")));
+            this.metroLink6.ImageSize = 20;
+            this.metroLink6.Location = new System.Drawing.Point(716, 92);
+            this.metroLink6.Name = "metroLink6";
+            this.metroLink6.Size = new System.Drawing.Size(36, 23);
+            this.metroLink6.TabIndex = 13;
+            this.metroLink6.UseSelectable = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(762, 608);
+            this.Controls.Add(this.metroLink6);
+            this.Controls.Add(this.metroTextBox3);
+            this.Controls.Add(this.metroTextBox2);
+            this.Controls.Add(this.metroLink5);
+            this.Controls.Add(this.metroButton3);
+            this.Controls.Add(this.metroButton2);
+            this.Controls.Add(this.metroTabControl1);
+            this.Controls.Add(this.metroLink4);
+            this.Controls.Add(this.metroLink3);
+            this.Controls.Add(this.metroButton1);
+            this.Controls.Add(this.metroTextBox1);
+            this.Controls.Add(this.metroLink2);
+            this.Controls.Add(this.metroLink1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Torchhh";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroLink metroLink1;
+        private MetroFramework.Controls.MetroLink metroLink2;
+        private MetroFramework.Controls.MetroTextBox metroTextBox1;
+        private MetroFramework.Controls.MetroButton metroButton1;
+        private MetroFramework.Controls.MetroLink metroLink3;
+        private MetroFramework.Controls.MetroLink metroLink4;
+        private MetroFramework.Controls.MetroTabControl metroTabControl1;
+        private MetroFramework.Controls.MetroButton metroButton2;
+        private MetroFramework.Controls.MetroButton metroButton3;
+        private MetroFramework.Controls.MetroLink metroLink5;
+        private MetroFramework.Controls.MetroTextBox metroTextBox2;
+        private MetroFramework.Controls.MetroTextBox metroTextBox3;
+        private MetroFramework.Controls.MetroLink metroLink6;
+    }
+}
+
---
+++ Torchhhhhhh/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Torchhhhhhh
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void NewTab()
+        {
+            TabPage tab = new TabPage();
+            tab.Text = "New tab";
+            metroTabControl1.Controls.Add(tab);
+            metroTabControl1.SelectTab(metroTabControl1.TabCount - 1);
+            WebBrowser browser = new WebBrowser() { ScriptErrorsSuppressed = true };
+            browser.Parent = tab;
+            browser.Dock = DockStyle.Fill;
+            browser.Navigate("https://www.google.com");
+            metroTextBox1.Text = "https://www.google.com";
+            browser.DocumentCompleted += browser_DocumentCompleted;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            NewTab();
+        }
+
+        void browser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            WebBrowser browser = metroTabControl1.SelectedTab.Controls[0] as WebBrowser;
+            if (browser != null)
+                metroTabControl1.SelectedTab.Text = browser.DocumentTitle;
+        }
+
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+            NewTab();
+        }
+
+        private void metroLink1_Click(object sender, EventArgs e)
+        {
+            WebBrowser browser = metroTabControl1.SelectedTab.Controls[0] as WebBrowser;
+            if (browser != null)
+            {
+                if (browser.CanGoBack)
+                    browser.GoBack();
+            }
+        }
+
+        private void metroLink2_Click(object sender, EventArgs e)
+        {
+            WebBrowser browser = metroTabControl1.SelectedTab.Controls[0] as WebBrowser;
+            if (browser != null)
+            {
+                if (browser.CanGoForward)
+                    browser.GoForward();
+            }
+        }
+
+        private void metroButton2_Click(object sender, EventArgs e)
+        {
+            WebBrowser browser = metroTabControl1.SelectedTab.Controls[0] as WebBrowser;
+            if (browser != null)
+                browser.Navigate(metroTextBox1.Text);
+        }
+
+
+       /// <summary>
+       /// this is the core search function 
+       /// </summary>
+        private void NavigateTopage()
+        {
+            
+        }
+
+        /// <summary>
+        /// this will fire when a key is pushed 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void metroTextBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)ConsoleKey.Enter)
+            {
+
+            }
+        }
+
+        private void metroLink3_Click(object sender, EventArgs e)
+        {
+            WebBrowser browser = metroTabControl1.SelectedTab.Controls[0] as WebBrowser;
+            if (browser != null)
+                browser.Navigate("https://www.google.com");
+        }
+
+        private void metroLink4_Click(object sender, EventArgs e)
+        {
+            WebBrowser browser = metroTabControl1.SelectedTab.Controls[0] as WebBrowser;
+            if (browser != null)
+                browser.Refresh();
+        }
+
+        private void metroButton3_Click(object sender, EventArgs e)
+        {
+            metroTabControl1.TabPages.RemoveAt(metroTabControl1.SelectedIndex);
+        }
+    }
+}
